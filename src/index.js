f29ad2458d9633879122476f3c962bdce553f572
CharrafiMed <mhch5003@gmail.com>
2024-07-09 00:05:09 +0200

    work on progress

--- src/index.js
+++ src/index.js
@@ -4,19 +4,35 @@ export default (Alpine) => {
   Alpine.directive(
     "animate",
     (el, { value, modifiers, expression }, { Alpine, effect, cleanup }) => {
-        console.log('here')
-        let configs={};
-      
-        // check for modifiers
+      console.log(modifiers);
+      console.log(expression);
+      console.log(Alpine);
+      console.log(effect);
+      console.log(cleanup);
+      console.log(value);
 
+      let configs = {};
+
+      // check for modifiers
+      if (modifiers.includes("easing")) {
+        console.log("easing exists");
+        keyIndex = modifiers.indexOf("easing");
+        valueIndex = modifiers[keyIndex + 1];
+      }
+      if (modifiers.includes("duration")) {
+        console.log("duration exists");
+      }
+      if (modifiers.includes("disrespectusermotionpreference")) {
+        console.log("disrespectUserMotionPreference exists");
+      }
       // grap the duration modifier
 
       // grap the ease modifier
-      
+
       // grap the disrespectUserMotionPreference
-      
+
       // or evaluate the passed object instead eg: { duration:300, ease:linear, disrespectUserMotionPreference}
-      
+
       // initialize autoAnimate(el,{configs}) and pass to it the params
     }
   );
