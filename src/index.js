8f1e2c59dd71a249a2d1eee1758f6e78ffceb1bd
CharrafiMed <mhch5003@gmail.com>
2024-07-09 13:28:11 +0200

    modifiers graping phase

--- src/index.js
+++ src/index.js
@@ -14,8 +14,28 @@ export default (Alpine) => {
       let configs = {};
       // handling the duration modifier
       if (modifiers.includes("duration")) {
-        console.log("duration exists");
+        const durationIndex = modifiers.indexOf("duration");
+        const durationValue = modifiers[durationIndex + 1];
+        const durationRegex = /^(\d+)(ms|s)?$/;
+        if (durationRegex.test(durationValue)) {
+          const match = durationRegex.exec(durationValue);
+          const durationNumber = parseInt(match[1], 10);
+
+          // Default to "ms" if unit is empty
+          const durationUnit = match[2] || "ms";
+
+          configs.duration =
+            durationUnit === "s" ? durationNumber * 1000 : durationNumber;
+          console.log(configs);
+        } else {
+          console.warn(
+            "Invalid duration format. Use digits followed by 'ms' or 's'."
+          );
+        }
+      } else {
+        console.warn('The "duration" modifier was specified without a value.');
       }
+
       // handling the easing modifier
       if (modifiers.includes("easing")) {
         const easingValue = modifiers[modifiers.indexOf("easing") + 1];
@@ -27,7 +47,11 @@ export default (Alpine) => {
       }
 
       if (modifiers.includes("disrespectusermotionpreference")) {
-        console.log("disrespectUserMotionPreference exists");
+        const userMotionPrefValue =
+          modifiers[modifiers.indexOf("disrespectusermotionpreference") + 1];
+        configs.disrespectUserMotionPreference = userMotionPrefValue
+          ? true
+          : false;
       }
       // grap the duration modifier
 
@@ -38,6 +62,7 @@ export default (Alpine) => {
       // or evaluate the passed object instead eg: { duration:300, ease:linear, disrespectUserMotionPreference}
 
       // initialize autoAnimate(el,{configs}) and pass to it the params
+      console.log(configs);
     }
   );
 };
